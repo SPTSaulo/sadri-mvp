30da4d6143c08fce2f41a783e16abf752d716a18
Antonio Aguado Campos <antonioaguadocampos@gmail.com>
2025-02-13 17:51:56 +0000

    [add coins fix]

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'sadri-mvp',
-  webDir: 'www'
+  appName: 'Sadri',
+  webDir: 'www',
 };
 
 export default config;
